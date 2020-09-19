4be5594b518aa0fda32456c5bf0ccaa6fb7b6465
Svellem <sverre80@gmail.com>
2020-09-19 14:21:04 +0200

    Update Configuration.h
    double stps per unit x,y,e (x2,y2)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 200 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 800, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
